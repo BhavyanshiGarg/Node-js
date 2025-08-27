e9b78f52fff10ef4cf4db2afba58fcb9937e4415
Bhavyanshi Garg <reetagarg2017@gmail.com>
2025-08-27 15:26:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//console.log("hello")
+//const data=require("fs")
+//data.writeFileSync("abc.txt","fjlfjslfjslf")node
+//const readData=data.readFileSync("abc.txt","utf-8")
+//console.log(readData)
+
+const data1=require("chalk")
+console.log(data1.green("tis is my frist module"))
+console.log(data1.bgBlue("sfsfsnode"))
